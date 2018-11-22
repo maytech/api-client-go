6fe9dc23384badd7faaa46c44bafe629bc6b9618
Bohdan Lisovskyi <www.lbohdan@gmail.com>
2018-11-22 13:26:26 +0000

    v1.0-80

--- validation.go
+++ validation.go
@@ -6,28 +6,8 @@ import (
     "strings"
 )
 
-// Validate IdsReq
-func (s IdsReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate AutomationCreateReq
-func (s AutomationCreateReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate ProfileSetMfaReq
-func (s ProfileSetMfaReq) Validate() error {
+// Validate PfeditUsersReq
+func (s PfeditUsersReq) Validate() error {
 
     if err := validate(&s); err != nil {
     		return err
@@ -46,138 +26,8 @@ func (s UserSetMfaReq) Validate() error {
 	return nil
 }
 
-// Validate UserRemoveMfaReq
-func (s UserRemoveMfaReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate ShareLoginPinReq
-func (s ShareLoginPinReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate ContactEditResp
-func (s ContactEditResp) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate PgpCreateReq
-func (s PgpCreateReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate ResetPasswordResetReq
-func (s ResetPasswordResetReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate PfaddUsersReq
-func (s PfaddUsersReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate PfeditUsersReq
-func (s PfeditUsersReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate UserCreateReq
-func (s UserCreateReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate FilesReturnSendReq
-func (s FilesReturnSendReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate FilesReturnUploadLinkReq
-func (s FilesReturnUploadLinkReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate QuicklinkCreateReq
-func (s QuicklinkCreateReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate FileAddTagReq
-func (s FileAddTagReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate ContactCreateReq
-func (s ContactCreateReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate SearchReq
-func (s SearchReq) Validate() error {
+// Validate IdsReq
+func (s IdsReq) Validate() error {
 
     if err := validate(&s); err != nil {
     		return err
@@ -196,8 +46,8 @@ func (s ProfileRemoveMfaReq) Validate() error {
 	return nil
 }
 
-// Validate SshKeyDeleteReq
-func (s SshKeyDeleteReq) Validate() error {
+// Validate PfSetUsersReq
+func (s PfSetUsersReq) Validate() error {
 
     if err := validate(&s); err != nil {
     		return err
@@ -206,8 +56,8 @@ func (s SshKeyDeleteReq) Validate() error {
 	return nil
 }
 
-// Validate UploadLinkReq
-func (s UploadLinkReq) Validate() error {
+// Validate ShareLoginPinReq
+func (s ShareLoginPinReq) Validate() error {
 
     if err := validate(&s); err != nil {
     		return err
@@ -216,128 +66,8 @@ func (s UploadLinkReq) Validate() error {
 	return nil
 }
 
-// Validate MakeDirReq
-func (s MakeDirReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate FileModifyReq
-func (s FileModifyReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate UserDeleteReq
-func (s UserDeleteReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate CopyMoveFilesReq
-func (s CopyMoveFilesReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate PgpEditReq
-func (s PgpEditReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate ProfileSetPasswordReq
-func (s ProfileSetPasswordReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate KeyRequestRespondReq
-func (s KeyRequestRespondReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate SessionLoginPostResp
-func (s SessionLoginPostResp) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate QuicklinkLoginPinReq
-func (s QuicklinkLoginPinReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate PfdeleteUsersReq
-func (s PfdeleteUsersReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate ResetPasswordRequestReq
-func (s ResetPasswordRequestReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate PfUsersListReq
-func (s PfUsersListReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate SettingsSetReq
-func (s SettingsSetReq) Validate() error {
+// Validate PfaddUsersReq
+func (s PfaddUsersReq) Validate() error {
 
     if err := validate(&s); err != nil {
     		return err
@@ -366,8 +96,108 @@ func (s ShareRequestReq) Validate() error {
 	return nil
 }
 
+// Validate ProfileSetPasswordReq
+func (s ProfileSetPasswordReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
 // Validate BillingUpgradeReq
 func (s BillingUpgradeReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate ProfileSetMfaReq
+func (s ProfileSetMfaReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate SshKeyCreateReq
+func (s SshKeyCreateReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate ShareDownloadLinkReq
+func (s ShareDownloadLinkReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate PfcreateReq
+func (s PfcreateReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate AutomationCreateReq
+func (s AutomationCreateReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate ResetPasswordResetReq
+func (s ResetPasswordResetReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate UserDeleteReq
+func (s UserDeleteReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate SettingsSetReq
+func (s SettingsSetReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate UserCreateReq
+func (s UserCreateReq) Validate() error {
 
     if err := validate(&s); err != nil {
     		return err
@@ -386,8 +216,8 @@ func (s ProfileSetReq) Validate() error {
 	return nil
 }
 
-// Validate PfSetUsersReq
-func (s PfSetUsersReq) Validate() error {
+// Validate PfUsersListReq
+func (s PfUsersListReq) Validate() error {
 
     if err := validate(&s); err != nil {
     		return err
@@ -396,8 +226,158 @@ func (s PfSetUsersReq) Validate() error {
 	return nil
 }
 
-// Validate FilesReturnMakedirReq
-func (s FilesReturnMakedirReq) Validate() error {
+// Validate FileModifyReq
+func (s FileModifyReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate PgpEditReq
+func (s PgpEditReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate ShareSendRequestReq
+func (s ShareSendRequestReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate SearchReq
+func (s SearchReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate FilesReturnUploadLinkReq
+func (s FilesReturnUploadLinkReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate PfdeleteUsersReq
+func (s PfdeleteUsersReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate QuicklinkLoginPinReq
+func (s QuicklinkLoginPinReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate WidgetUploadLinkReq
+func (s WidgetUploadLinkReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate FilesReturnSendReq
+func (s FilesReturnSendReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate AutomationEditReq
+func (s AutomationEditReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate SshKeyEditReq
+func (s SshKeyEditReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate PgpCreateReq
+func (s PgpCreateReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate QuicklinkCreateReq
+func (s QuicklinkCreateReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate SshKeyDeleteReq
+func (s SshKeyDeleteReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate CopyMoveFilesReq
+func (s CopyMoveFilesReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate UserRemoveMfaReq
+func (s UserRemoveMfaReq) Validate() error {
 
     if err := validate(&s); err != nil {
     		return err
@@ -426,8 +406,18 @@ func (s FileMetadataPostReq) Validate() error {
 	return nil
 }
 
-// Validate ShareDownloadLinkReq
-func (s ShareDownloadLinkReq) Validate() error {
+// Validate MakeDirReq
+func (s MakeDirReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate FilesReturnMakedirReq
+func (s FilesReturnMakedirReq) Validate() error {
 
     if err := validate(&s); err != nil {
     		return err
@@ -446,8 +436,8 @@ func (s UserSignupReq) Validate() error {
 	return nil
 }
 
-// Validate WidgetUploadLinkReq
-func (s WidgetUploadLinkReq) Validate() error {
+// Validate ContactEditResp
+func (s ContactEditResp) Validate() error {
 
     if err := validate(&s); err != nil {
     		return err
@@ -456,8 +446,8 @@ func (s WidgetUploadLinkReq) Validate() error {
 	return nil
 }
 
-// Validate ShareSendRequestReq
-func (s ShareSendRequestReq) Validate() error {
+// Validate SessionLoginPostResp
+func (s SessionLoginPostResp) Validate() error {
 
     if err := validate(&s); err != nil {
     		return err
@@ -466,8 +456,8 @@ func (s ShareSendRequestReq) Validate() error {
 	return nil
 }
 
-// Validate SshKeyEditReq
-func (s SshKeyEditReq) Validate() error {
+// Validate ShareCreateReq
+func (s ShareCreateReq) Validate() error {
 
     if err := validate(&s); err != nil {
     		return err
@@ -476,18 +466,8 @@ func (s SshKeyEditReq) Validate() error {
 	return nil
 }
 
-// Validate PfcreateReq
-func (s PfcreateReq) Validate() error {
-
-    if err := validate(&s); err != nil {
-    		return err
-    }
-
-	return nil
-}
-
-// Validate AutomationEditReq
-func (s AutomationEditReq) Validate() error {
+// Validate KeyRequestRespondReq
+func (s KeyRequestRespondReq) Validate() error {
 
     if err := validate(&s); err != nil {
     		return err
@@ -506,8 +486,8 @@ func (s UnblockCaptchaReq) Validate() error {
 	return nil
 }
 
-// Validate SshKeyCreateReq
-func (s SshKeyCreateReq) Validate() error {
+// Validate ResetPasswordRequestReq
+func (s ResetPasswordRequestReq) Validate() error {
 
     if err := validate(&s); err != nil {
     		return err
@@ -516,8 +496,28 @@ func (s SshKeyCreateReq) Validate() error {
 	return nil
 }
 
-// Validate ShareCreateReq
-func (s ShareCreateReq) Validate() error {
+// Validate UploadLinkReq
+func (s UploadLinkReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate ContactCreateReq
+func (s ContactCreateReq) Validate() error {
+
+    if err := validate(&s); err != nil {
+    		return err
+    }
+
+	return nil
+}
+
+// Validate FileAddTagReq
+func (s FileAddTagReq) Validate() error {
 
     if err := validate(&s); err != nil {
     		return err
